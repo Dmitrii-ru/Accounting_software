a3758787323dae47d60756646db6c784573ac973
nochvev1@mail.ru <nochev1@mail.ru>
2022-11-01 00:56:20 +0300

    site mainloop

--- Code/main.py
+++ Code/main.py
@@ -4,7 +4,7 @@ import tkinter
 from Nalog import nal, result
 # sudo apt install python3-tk
 win_main = tkinter.Tk()
-win_main.geometry(f'200x70+10+20')
+win_main.geometry(f'400x100+10+20')
 win_main.config(bg='#20FC9D')
 win_main.title("Бух")
 btn1 = tkinter.Button(win_main, text="Налоги 1.Грузим налоги 2. Грузим больничный ", command=nal, bg="#20FCED", pady=0.5, padx=0.5).grid(row=6, column=1)
